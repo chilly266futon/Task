2991101af41c521a3bfea08eee477c988c9f0c4f
k.rogachev <k.rogachev@realize.dev>
2024-10-11 13:45:47 +0300

    update test

--- test/main_test.go
+++ test/main_test.go
@@ -14,40 +14,42 @@ func Test_MaxNum(t *testing.T) {
 	}{
 		{
 			name: "Test 1",
-			a:    57.0,
-			b:    9.0,
-			want: 57.0,
+			a:    10,
+			b:    20,
+			want: 20,
 		},
 		{
 			name: "Test 2",
-			a:    0.0,
-			b:    0.0,
-			want: 0.0,
+			a:    20,
+			b:    10,
+			want: 20,
 		},
 		{
 			name: "Test 3",
-			a:    -1.0,
-			b:    -2.0,
-			want: -1.0,
+			a:    0,
+			b:    0,
+			want: 0,
 		},
 		{
 			name: "Test 4",
-			a:    17.0,
-			b:    23.0,
-			want: 23.0,
+			a:    -10,
+			b:    -20,
+			want: -10,
 		},
 		{
 			name: "Test 5",
-			a:    0.0,
-			b:    1.0,
-			want: 1.0,
+			a:    -20,
+			b:    -10,
+			want: -10,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := MaxNum(tt.a, tt.b); got != tt.want {
+			got := MaxNum(tt.a, tt.b)
+			if got != tt.want {
 				t.Errorf("MaxNum() = %v, want %v", got, tt.want)
 			}
+			t.Logf("MaxNum(%v, %v) = %v", tt.a, tt.b, got)
 		})
 	}
 }
