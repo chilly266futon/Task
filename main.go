c7b0c003f7476b8109e5f45876a77914a0f8bd7e
k.rogachev <k.rogachev@realize.dev>
2024-10-11 09:38:14 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"math"
+)
+
+// MaxNum returns the maximum of two float64 numbers.
+// It uses the formula (a + b + |a-b|) / 2 to determine the maximum value.
+//
+// Parameters:
+//   - a: the first float64 number.
+//   - b: the second float64 number.
+//
+// Returns:
+//   - The maximum of the two float64 numbers.
+func MaxNum(a, b float64) float64 {
+	res := (a + b + math.Abs(a-b)) / 2
+	return res
+}
---
+++ test/main_test.go
@@ -0,0 +1,10 @@
+package test
+
+import "testing"
+
+func Test_MaxNum(t *testing.T) {
+	a := 57.0
+	b := 9.0
+	res := MaxNum(a, b)
+	t.Log(res)
+}
